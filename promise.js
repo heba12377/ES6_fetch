62e12d28d18f7984e1e6423dc25885d1af64b57f
Heba El-Said <71638009+heba12377@users.noreply.github.com>
2023-01-02 00:54:21 +0200

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,46 +1,4 @@
 
-// var promUser = new Promise((res, rej) => {
-//     setTimeout(()=>{
-//         var promiseCarriesResponse = fetch("https://jsonplaceholder.typicode.com/users");
-//         if (promiseCarriesResponse) {
-//             //console.log(promiseCarriesResponse);
-//             res(promiseCarriesResponse);
-//         }
-//         else{rej("Users data not found");}
-//     },1000);
-// });
-
-// function getPosts(user) {
-//     var promPosts = new Promise ((res,rej)=>{
-//         setTimeout(()=>{
-//             var posts = fetch("https://jsonplaceholder.typicode.com/posts?userId="+user+"");
-//             console.log(posts);
-//             //var posts = fetch("https://jsonplaceholder.typicode.com/posts?userId="+user[id]+"");
-//             if(posts){
-//                 res(posts);
-//             }
-//             else{"Posts not found"}
-//         },1000);
-//     });
-//     return promPosts;
-// }
-
-// async function myPromises(userId) {
-//     try {
-//         var x = await promUser;
-//         console.log(x);
-//         var y = x.json();
-//         console.log(y); 
-//     } catch (error) {console.log(error);}
-// }
-
-//promUser.then((responseFromUserPromise)=>responseFromUserPromise.json()) // promise ( result --> data of users )
-// promiseCarriesResponse.then((response)=>response.json()) //promise ( result --> data of users )
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 async function getUsers() {
     var promiseCarriesResponse = await fetch("https://jsonplaceholder.typicode.com/users");
     return promiseCarriesResponse;
